03c3f6b76251a9284a7bed75adfbdd066217a563
ankitjha420 <ankitjha11thegreat@gmail.com>
2023-07-07 23:54:19 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// import statements
+import { Canvas } from '@react-three/fiber'
+import { 
+  OrbitControls,
+  Center,
+  useGLTF, 
+  AccumulativeShadows,
+  RandomizedLight,
+  Environment } from '@react-three/drei'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = (
+  {position = [-1, 0, 2.5], fov = 25}
+) => {
+  return(
+    <Canvas shadows
+      camera={{position, fov}}
+      eventSource={document.getElementById('root')}
+      eventPrefix='client'>
+      <ambientLight intensity={0.5}/>
+      <Center>
+        <Environment preset='city'/>
+        <Shirt />
+        <Backdrop />
+      </Center>
+      <OrbitControls />
+    </Canvas>
+  )
 }
 
-export default App;
+function Shirt(props) {
+  const { nodes, materials } = useGLTF('/shirt.glb')
+  return (
+      <group {...props} dispose={null}>
+        <mesh geometry={nodes.T_Shirt_male.geometry} material={materials.lambert1} position={[0.419, 0, 0]} rotation={[Math.PI / 2, 0, 0]} />
+      </group>
+  )
+}
+
+useGLTF.preload('/shirt.glb')
+
+function Backdrop() {
+  return(
+    <AccumulativeShadows
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={100}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.2]}>
+      <RandomizedLight 
+        amount={4}
+        radius={0}
+        intensity={0.2}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
+      <RandomizedLight 
+        amount={4}
+        radius={5}
+        intensity={0.2}
+        ambient={0.55}
+        position={[-5, 5, -9]}
+      />
+    </AccumulativeShadows>
+  )
+}
